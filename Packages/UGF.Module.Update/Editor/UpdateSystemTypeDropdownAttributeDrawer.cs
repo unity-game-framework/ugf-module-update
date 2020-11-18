38f87d14ebfacfccc4c3739f208c97e066035720
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-11-18 20:36:23 +0300

    Add UpdateSystemTypeDropdownAttributeDrawer

---
+++ Packages/UGF.Module.Update/Editor/UpdateSystemTypeDropdownAttributeDrawer.cs
@@ -0,0 +1,19 @@
+﻿using UGF.EditorTools.Editor.IMGUI.PropertyDrawers;
+using UGF.Module.Update.Runtime;
+using UnityEditor;
+using UnityEngine;
+
+namespace UGF.Module.Update.Editor
+{
+    [CustomPropertyDrawer(typeof(UpdateSystemTypeDropdownAttribute), true)]
+    internal class UpdateSystemTypeDropdownAttributeDrawer : PropertyDrawerTyped<UpdateSystemTypeDropdownAttribute>
+    {
+        public UpdateSystemTypeDropdownAttributeDrawer() : base(SerializedPropertyType.String)
+        {
+        }
+
+        protected override void OnDrawProperty(Rect position, SerializedProperty serializedProperty, GUIContent label)
+        {
+        }
+    }
+}
