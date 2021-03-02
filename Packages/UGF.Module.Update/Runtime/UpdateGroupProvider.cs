7b2d9713eee405491f7b7c0c249f039376492af6
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-03-02 21:50:26 +0300

    Add UpdateGroupProvider and UpdateSystemDescriptionProvider

---
+++ Packages/UGF.Module.Update/Runtime/UpdateGroupProvider.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using UGF.Description.Runtime;
+using UGF.Logs.Runtime;
+using UGF.RuntimeTools.Runtime.Providers;
+using UGF.Update.Runtime;
+
+namespace UGF.Module.Update.Runtime
+{
+    public class UpdateGroupProvider : Provider<string, IUpdateGroup>
+    {
+        public IUpdateProvider UpdateProvider { get; }
+
+        public UpdateGroupProvider(IUpdateProvider updateProvider) : this(updateProvider, EqualityComparer<string>.Default)
+        {
+        }
+
+        public UpdateGroupProvider(IUpdateProvider updateProvider, IEqualityComparer<string> comparer) : base(comparer)
+        {
+            UpdateProvider = updateProvider ?? throw new ArgumentNullException(nameof(updateProvider));
+        }
+
+        protected override void OnAdd(string id, IUpdateGroup entry)
+        {
+            if (!(entry is IDescribed<IUpdateGroupDescription> described)) throw new ArgumentException("Entry must be of 'IDescribed<IUpdateGroupDescription>' type.", nameof(entry));
+
+            UpdateProvider.Add(described.Description.SystemType, entry);
+
+            base.OnAdd(id, entry);
+
+            Log.Debug("Add update group", new
+            {
+                id,
+                entry.Name,
+                described.Description.SystemType
+            });
+        }
+
+        protected override bool OnRemove(string id, IUpdateGroup entry)
+        {
+            UpdateProvider.Remove(entry);
+
+            Log.Debug("Remove update group", new
+            {
+                id,
+                entry.Name
+            });
+
+            return base.OnRemove(id, entry);
+        }
+    }
+}
---
+++ Packages/UGF.Module.Update/Runtime/UpdateSystemDescriptionProvider.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using UGF.Logs.Runtime;
+using UGF.RuntimeTools.Runtime.Providers;
+using UGF.Update.Runtime;
+
+namespace UGF.Module.Update.Runtime
+{
+    public class UpdateSystemDescriptionProvider : Provider<string, IUpdateSystemDescription>
+    {
+        public IUpdateProvider UpdateProvider { get; }
+
+        public UpdateSystemDescriptionProvider(IUpdateProvider updateProvider) : this(updateProvider, EqualityComparer<string>.Default)
+        {
+        }
+
+        public UpdateSystemDescriptionProvider(IUpdateProvider updateProvider, IEqualityComparer<string> comparer) : base(comparer)
+        {
+            UpdateProvider = updateProvider ?? throw new ArgumentNullException(nameof(updateProvider));
+        }
+
+        protected override void OnAdd(string id, IUpdateSystemDescription entry)
+        {
+            UpdateProvider.UpdateLoop.Add(entry.TargetSystemType, entry.SystemType, entry.Insertion);
+
+            base.OnAdd(id, entry);
+
+            Log.Debug("Add update system", new
+            {
+                id,
+                entry.TargetSystemType,
+                entry.SystemType,
+                entry.Insertion
+            });
+        }
+
+        protected override bool OnRemove(string id, IUpdateSystemDescription entry)
+        {
+            UpdateProvider.UpdateLoop.Remove(entry.SystemType);
+
+            Log.Debug("Remove update system", new
+            {
+                id,
+                entry.TargetSystemType,
+                entry.SystemType,
+                entry.Insertion
+            });
+
+            return base.OnRemove(id, entry);
+        }
+    }
+}
