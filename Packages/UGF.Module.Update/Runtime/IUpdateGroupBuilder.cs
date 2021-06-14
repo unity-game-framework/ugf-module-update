641ea01b24c148c3dc1286d6f762f6f3fa9d6fbe
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-06-14 18:28:41 +0300

    Change IUpdateGroupBuilder

--- Packages/UGF.Module.Update/Runtime/IUpdateGroupBuilder.cs
+++ Packages/UGF.Module.Update/Runtime/IUpdateGroupBuilder.cs
@@ -1,9 +1,10 @@
-﻿using UGF.Builder.Runtime;
+﻿using UGF.Application.Runtime;
+using UGF.Builder.Runtime;
 using UGF.Update.Runtime;
 
 namespace UGF.Module.Update.Runtime
 {
-    public interface IUpdateGroupBuilder : IBuilder<IUpdateGroup>
+    public interface IUpdateGroupBuilder : IBuilder<IApplication, IUpdateGroup>
     {
     }
 }
