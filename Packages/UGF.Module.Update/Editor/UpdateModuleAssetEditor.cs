cda4785cda227ee9709a636db911720977321613
Vorobey <vorobyovvorobyov@gmail.com>
2023-01-04 23:44:07 +0100

    Change editors

--- Packages/UGF.Module.Update/Editor/UpdateModuleAssetEditor.cs
+++ Packages/UGF.Module.Update/Editor/UpdateModuleAssetEditor.cs
@@ -1,3 +1,4 @@
+using UGF.EditorTools.Editor.IMGUI;
 using UGF.EditorTools.Editor.IMGUI.Scopes;
 using UGF.Module.Update.Runtime;
 using UnityEditor;
@@ -7,47 +8,90 @@ namespace UGF.Module.Update.Editor
     [CustomEditor(typeof(UpdateModuleAsset), true)]
     internal class UpdateModuleAssetEditor : UnityEditor.Editor
     {
-        private SerializedProperty m_propertyScript;
-        private UpdateModuleAssetEditorSystemEntryListDrawer m_listSystems;
-        private UpdateModuleAssetGroupEntryListDrawer m_listGroups;
-        private UpdateModuleAssetBuilderEntryListDrawer m_listSubGroups;
-        private UpdateModuleAssetBuilderEntryListDrawer m_listEntries;
+        private UpdateModuleAssetSystemListDrawer m_listSystems;
+        private ReorderableListKeyAndValueDrawer m_listGroups;
+        private ReorderableListSelectionDrawerByPath m_listGroupsSelection;
+        private ReorderableListKeyAndValueDrawer m_listSubGroups;
+        private ReorderableListSelectionDrawerByPathGlobalId m_listSubGroupsSelectionGroup;
+        private ReorderableListSelectionDrawerByPath m_listSubGroupsSelectionBuilder;
+        private ReorderableListKeyAndValueDrawer m_listEntries;
+        private ReorderableListSelectionDrawerByPathGlobalId m_listEntriesSelectionGroup;
+        private ReorderableListSelectionDrawerByPath m_listEntriesSelectionBuilder;
 
         private void OnEnable()
         {
-            m_propertyScript = serializedObject.FindProperty("m_Script");
-            m_listSystems = new UpdateModuleAssetEditorSystemEntryListDrawer(serializedObject.FindProperty("m_systems"));
-            m_listGroups = new UpdateModuleAssetGroupEntryListDrawer(serializedObject.FindProperty("m_groups"));
-            m_listSubGroups = new UpdateModuleAssetBuilderEntryListDrawer(serializedObject.FindProperty("m_subGroups"));
-            m_listEntries = new UpdateModuleAssetBuilderEntryListDrawer(serializedObject.FindProperty("m_entries"));
+            m_listSystems = new UpdateModuleAssetSystemListDrawer(serializedObject.FindProperty("m_systems"));
+            m_listGroups = new ReorderableListKeyAndValueDrawer(serializedObject.FindProperty("m_groups"), "m_subSystemType", "m_group");
+
+            m_listGroupsSelection = new ReorderableListSelectionDrawerByPath(m_listGroups, "m_group.m_asset")
+            {
+                Drawer = { DisplayTitlebar = true }
+            };
+
+            m_listSubGroups = new ReorderableListKeyAndValueDrawer(serializedObject.FindProperty("m_subGroups"), "m_group", "m_builder");
+
+            m_listSubGroupsSelectionGroup = new ReorderableListSelectionDrawerByPathGlobalId(m_listSubGroups, "m_group")
+            {
+                Drawer = { DisplayTitlebar = true }
+            };
+
+            m_listSubGroupsSelectionBuilder = new ReorderableListSelectionDrawerByPath(m_listSubGroups, "m_builder.m_asset")
+            {
+                Drawer = { DisplayTitlebar = true }
+            };
+
+            m_listEntries = new ReorderableListKeyAndValueDrawer(serializedObject.FindProperty("m_entries"), "m_group", "m_builder");
+
+            m_listEntriesSelectionGroup = new ReorderableListSelectionDrawerByPathGlobalId(m_listSubGroups, "m_group")
+            {
+                Drawer = { DisplayTitlebar = true }
+            };
+
+            m_listEntriesSelectionBuilder = new ReorderableListSelectionDrawerByPath(m_listSubGroups, "m_builder.m_asset")
+            {
+                Drawer = { DisplayTitlebar = true }
+            };
 
             m_listSystems.Enable();
             m_listGroups.Enable();
+            m_listGroupsSelection.Enable();
             m_listSubGroups.Enable();
+            m_listSubGroupsSelectionGroup.Enable();
+            m_listSubGroupsSelectionBuilder.Enable();
             m_listEntries.Enable();
+            m_listEntriesSelectionGroup.Enable();
+            m_listEntriesSelectionBuilder.Enable();
         }
 
         private void OnDisable()
         {
             m_listSystems.Disable();
             m_listGroups.Disable();
+            m_listGroupsSelection.Disable();
             m_listSubGroups.Disable();
+            m_listSubGroupsSelectionGroup.Disable();
+            m_listSubGroupsSelectionBuilder.Disable();
             m_listEntries.Disable();
+            m_listEntriesSelectionGroup.Disable();
+            m_listEntriesSelectionBuilder.Disable();
         }
 
         public override void OnInspectorGUI()
         {
             using (new SerializedObjectUpdateScope(serializedObject))
             {
-                using (new EditorGUI.DisabledScope(true))
-                {
-                    EditorGUILayout.PropertyField(m_propertyScript);
-                }
+                EditorIMGUIUtility.DrawScriptProperty(serializedObject);
 
                 m_listSystems.DrawGUILayout();
                 m_listGroups.DrawGUILayout();
                 m_listSubGroups.DrawGUILayout();
                 m_listEntries.DrawGUILayout();
+
+                m_listGroupsSelection.DrawGUILayout();
+                m_listSubGroupsSelectionGroup.DrawGUILayout();
+                m_listSubGroupsSelectionBuilder.DrawGUILayout();
+                m_listEntriesSelectionGroup.DrawGUILayout();
+                m_listEntriesSelectionBuilder.DrawGUILayout();
             }
         }
     }
---
+++ Packages/UGF.Module.Update/Editor/UpdateModuleAssetSystemListDrawer.cs
@@ -0,0 +1,40 @@
+﻿using UGF.EditorTools.Editor.IMGUI;
+using UGF.EditorTools.Editor.IMGUI.Scopes;
+using UnityEditor;
+using UnityEngine;
+
+namespace UGF.Module.Update.Editor
+{
+    internal class UpdateModuleAssetSystemListDrawer : ReorderableListDrawer
+    {
+        public UpdateModuleAssetSystemListDrawer(SerializedProperty serializedProperty) : base(serializedProperty)
+        {
+        }
+
+        protected override void OnDrawElementContent(Rect position, SerializedProperty serializedProperty, int index, bool isActive, bool isFocused)
+        {
+            SerializedProperty propertyTargetSystemType = serializedProperty.FindPropertyRelative("m_targetSystemType");
+            SerializedProperty propertySystemType = serializedProperty.FindPropertyRelative("m_systemType");
+            SerializedProperty propertyInsertion = serializedProperty.FindPropertyRelative("m_insertion");
+
+            float height = EditorGUIUtility.singleLineHeight;
+            float space = EditorGUIUtility.standardVerticalSpacing;
+
+            var rectTargetSystemType = new Rect(position.x, position.y, position.width, height);
+            var rectSystemType = new Rect(position.x, rectTargetSystemType.yMax + space, position.width, height);
+            var rectInsertion = new Rect(position.x, rectSystemType.yMax + space, position.width, height);
+
+            using (new IndentIncrementScope(-1))
+            {
+                EditorGUI.PropertyField(rectTargetSystemType, propertyTargetSystemType);
+                EditorGUI.PropertyField(rectSystemType, propertySystemType);
+                EditorGUI.PropertyField(rectInsertion, propertyInsertion);
+            }
+        }
+
+        protected override float OnElementHeightContent(SerializedProperty serializedProperty, int index)
+        {
+            return EditorGUIUtility.singleLineHeight * 3F + EditorGUIUtility.standardVerticalSpacing * 2F;
+        }
+    }
+}
